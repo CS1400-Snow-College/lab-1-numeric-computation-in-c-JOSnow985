7fc75651720e28a52d242c15ca1cb3181540aa4f
JSnow985 <jaden.olvera@students.snow.edu>
2025-09-03 15:56:29 -0600

    renumbered steps to be accurate to assignment

--- Program.cs
+++ Program.cs
@@ -3,14 +3,13 @@ using System.Formats.Asn1;
 
 Console.Clear();
 
-// Step 1: Integer Math 1
+// Step 1: Integer Math
 Console.WriteLine("18 + 6 =");
 int a = 18;
 int b = 6;
 int c = a + b;
 Console.WriteLine(c);
 
-// Step 2: Integer Math 2
 // Remember to update which variable is writing!
 // Can't use 'int c' again, just 'c =' instead
 Console.WriteLine("18 - 6 =");
@@ -21,7 +20,7 @@ Console.WriteLine("18 * 6 =");
 c = a * b;
 Console.WriteLine(c);
 
-// Step 3: Order of Operations 1
+// Step 2: Order of Operations
 Console.WriteLine("5 + 4 * 2 =");
 a = 5;
 b = 4;
@@ -29,7 +28,6 @@ c = 2;
 int d = a + b * c;
 Console.WriteLine(d);
 
-// Step 4: Order of Operations 2
 Console.WriteLine("(5 + 4) * 2 =");
 a = 5;
 b = 4;
@@ -37,7 +35,6 @@ c = 2;
 d = (a + b) * c;
 Console.WriteLine(d);
 
-// Step 5: Order of Operations 3
 Console.WriteLine("(5 + 4) - 6 * 2 + (12 * 4) / 3 + 12 =");
 a = 5;
 b = 4;
@@ -45,7 +42,6 @@ c = 2;
 d = (a + b) - 6 * c + (12 * 4) / 3 + 12;
 Console.WriteLine(d);
 
-// Step 6: Order of Operations 4
 // Integer precision example
 Console.WriteLine("(7 + 4) / 3 =");
 a = 7;
@@ -54,7 +50,7 @@ c = 3;
 d = (a + b) / c;
 Console.WriteLine(d);
 
-// Step 7: Integer Precision and Limits 1
+// Step 3: Integer Precision and Limits
 // Can place a $ before the string to use {} instead of inputting the literal numbers?
 Console.WriteLine($"({a} + {b}) / {c} =");
 a = 7;
@@ -67,16 +63,14 @@ Console.WriteLine($"({a} + {b}) % {c} =");
 int e = (a + b) % c;
 Console.WriteLine($"remainder: {e}");
 
-// Step 8: Integer Precision and Limits 2
 int max = int.MaxValue;
 int min = int.MinValue;
 Console.WriteLine($"The range of integers is {min} to {max}!");
 
-// Step 9: Integer Precision and Limits 3
 int what = max + 3;
 Console.WriteLine($"An example of overflow: {what}");
 
-// Step 10: Double Type 1
+// Step 4: Double Type
 // can't redefine a so using aa instead
 // Obviously, can't use 
 double aa = 5;
@@ -86,7 +80,6 @@ Console.WriteLine($"Using double type instead, ({aa} + {bb}) / {cc}");
 double dd = (aa + bb) / cc;
 Console.WriteLine(dd);
 
-// Step 11: Double Type 2
 aa = 19;
 bb = 23;
 cc = 8;
@@ -94,18 +87,16 @@ Console.WriteLine($"Double type with bigger numbers! ({aa} + {bb}) / {cc}");
 dd = (aa + bb) / cc;
 Console.WriteLine(dd);
 
-// Step 12: Double Type 3
 // max and min already defined for int, using d as a prefix
 double dmax = double.MaxValue;
 double dmin = double.MinValue;
 Console.WriteLine($"The range of double is {dmin} to {dmax}!");
 
-// Step 13: Double Type 4
 Console.WriteLine("1.0 / 3.0 =");
 double third = 1.0 / 3.0;
 Console.WriteLine(third);
 
-// Step 14: Double Type Challenge
+// Double Type Challenge
 Console.WriteLine();
 aa = 220;
 bb = 60;
@@ -115,12 +106,11 @@ double ee = (aa * dd) + (aa / bb) + (cc * bb) - aa;
 Console.WriteLine($"({aa} * {dd}) + ({aa} / {bb}) + ({cc} * {bb}) - {aa} =");
 Console.WriteLine(ee);
 
-// Step 15: Decimal Types 1
+// Step 5: Decimal Types
 decimal decmin = decimal.MinValue;
 decimal decmax = decimal.MaxValue;
 Console.WriteLine($"The range of the decimal type is {decmin} to {decmax}!");
 
-// Step 16: Decimal Types 2
 aa = 1.0;
 bb = 3.0;
 Console.WriteLine("Using double, 1.0 / 3.0 =");
@@ -131,10 +121,10 @@ decimal decb = 3.0M;
 Console.WriteLine("Using decimal, 1.0 / 3.0 =");
 Console.WriteLine(deca / decb);
 
-// Step 17: Area of a Circle
+// Step 6: Area of a Circle Challenge
 // A = pi * r^2
 // area = pi * (r * r)
-double r = 2.5;
+double r = 2.50;
 double area = Math.PI * (r * r);
 Console.WriteLine("The area of a circle with a radius of 2.5 centimeters is, in double type, is");
 Console.WriteLine(area);
